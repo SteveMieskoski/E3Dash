73eeeaa3144822f50f9349b07a7ba76408f2ee5c
thatn3wguy <SteveMieskoski@gmail.com>
2017-07-24 13:50:40 -0400

    view result check

--- dashPart.js
+++ dashPart.js
@@ -1,4 +1,4 @@
-$('document').ready(function() {
+$('document').ready(function () {
 
 
 	var userData = localStorage.getItem('user_data');
@@ -8,4 +8,22 @@ $('document').ready(function() {
 	$('#wellsExample').attr('action', userData.wellsExample);
 	$('#allStockExample').attr('action', userData.allStockExample);
 
-})
+	//$('#project').text(userData.project[0].project);
+	//$('#version').text(userData.project[0].version);
+	//$('#download').attr('action', userData.project[0].download);
+
+	for(var i=0; i<userData.project.length; i++){
+		var template = '<tr>' +
+			'<td >' + userData.project[i].project + '</td>' +
+			'<td >' + userData.project[i].version +'</td>' +
+			'<td >' +
+			'<form  method="get" action="'+ userData.project[i].download +'">' +
+			'<button type="submit">Download!</button>' +
+			'</form>' +
+			'</td>' +
+			'</tr>';
+		$('#projectTable').append(template)
+	}
+
+
+});
